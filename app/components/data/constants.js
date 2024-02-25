a009d0a32480c4824b6e0b25fe8fb671e0f5e888
AyushScripts <ayushtheking117@gmail.com>
2024-02-25 20:54:58 +0530

    Updated icons

--- app/components/data/constants.js
+++ app/components/data/constants.js
@@ -85,7 +85,7 @@
         {
           name: "Figma",
           image:
-          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+          "https://cdn.freebiesupply.com/logos/large/2x/figma-1-logo-png-transparent.png",
         },
         {
           name: "Canva",
@@ -101,7 +101,7 @@
         {
           name: "Adobe XD",
           image:
-            "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+            "https://download.logo.wine/logo/Adobe_XD/Adobe_XD-Logo.wine.png",
         },
         
       ],
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.w3.org', 'upload.wikimedia.org', 'getbootstrap.com', 'www.drupal.org', 'cdn.icon-icons.com', 's3-alpha.figma.com', 'uxwing.com', 'camo.githubusercontent.com', 'e7.pngegg.com', 'github.githubassets.com', 'w7.pngwing.com', 'seeklogo.com', 'static-00.iconduck.com']
+        domains: ['cdn.freebiesupply.com', 'download.logo.wine', 'uxwing.com', 'www.w3.org', 'upload.wikimedia.org', 'getbootstrap.com', 'www.drupal.org', 'cdn.icon-icons.com', 's3-alpha.figma.com', 'uxwing.com', 'camo.githubusercontent.com', 'e7.pngegg.com', 'github.githubassets.com', 'w7.pngwing.com', 'seeklogo.com', 'static-00.iconduck.com']
     }
 };
 
