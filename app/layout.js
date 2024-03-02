2932049c2b06f9ff0371715bde0347547fa3e189
AyushScripts <ayushtheking117@gmail.com>
2024-03-02 20:27:57 +0530

    Updated OG image

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Ayush B- Programmer & Full-stack Web Developer",
   description: "Portfolio of a passionate front-end dev and an AI enthusiast.",
-  images: {
+  openGraph: {
+    images: {
       url: './images/OG-img.jpg',
-    },
+    }
+  },
+
 };
 
 export default function RootLayout({ children }) {
