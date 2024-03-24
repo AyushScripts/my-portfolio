a5a61a5e9fdc44bfee03db4ff54fca2b9d4f5ec6
AyushScripts <ayushtheking117@gmail.com>
2024-03-24 22:59:31 +0530

    Added social links to footer

--- app/components/ContactMe.jsx
+++ app/components/ContactMe.jsx
@@ -7,7 +7,11 @@ const ContactMe = () => {
     <div className='flex flex-col gap-5 py-5 mt-20 text-center'>
         <h1 className='text-[#333333] dark:text-white text-3xl lg:text-5xl font-serif'>Let's connect!</h1>
         <p className='text-[#817d7d] dark:text-[#adb7be] text-lg md:text-xl'>I am open to exciting opportunities. So if you have one, I am just a message away!</p>
-        <a className='text-base md:text-lg mx-auto text-white dark:text-[black] font-semibold bg-purple-400/90 hover:bg-purple-400 transition-all ease-in px-4 py-3 mt-4 rounded-lg' href="mailto:ayushtheking117@gmail.com">Contact Me!</a>
+        <a className='text-base md:text-lg mx-auto text-white dark:text-[black] font-semibold bg-purple-400/90 hover:bg-purple-400 transition-all ease-in px-4 py-3 mt-4 rounded-lg' href="mailto:ayushtheking117@gmail.com">Shoot Me a Message!</a>
+        <div className='mx-auto md:mt-4'>
+          <SocialLinks></SocialLinks>
+        </div>
+        
     </div>
   )
 }
