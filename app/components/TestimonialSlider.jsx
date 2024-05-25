420f1c716b6c6b2d385452a81f5e7d9d92065cd3
AyushScripts <ayushtheking117@gmail.com>
2024-05-25 20:24:15 +0530

    Updated Testimonial

--- app/components/TestimonialSlider.jsx
+++ app/components/TestimonialSlider.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
+
 
 const testimonials = [
   {
